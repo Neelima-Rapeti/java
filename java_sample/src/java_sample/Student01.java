9558a50013ac4f2692f72feb0ba2d8ed35ca2a2f
Neelima Rapeti <neelimarapeti@gmail.com>
2022-11-10 14:53:03 +0100

    ccc

--- java_sample/src/java_sample/Student01.java
+++ java_sample/src/java_sample/Student01.java
@@ -1,10 +1,11 @@
-package net.codejava;
+package java_sample;
 
-public class Helloworld {
+public class Student01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world"); 
+		System.out.println("hello");
+
 	}
 
 }
